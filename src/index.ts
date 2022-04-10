8a198a44360a9a7caf6101e7dba1a63d9dd4ef1b
Roland Szarka-Kovacs <Roland.Szarka-Kovacs@meshx.co>
2022-04-10 23:49:39 +0200

    patch: fix naming in sdk exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 // NOTE: Aliases to classes and interfaces are used for external consumption.
 
 // SDK exports.
+import Document from "./models/document"
 import ISigner from "./interfaces/signer"
-import IonDocumentModel from "./models/document"
-import IonPublicKeyModel from "./models/public-key"
-import IonServiceModel from "./models/service"
 import JwkEd25519 from "./models/jwk-ed25519"
 import JwkEs256k from "./models/jwk-es256k"
 import LocalSigner from "./local-signer"
+import PublicKey from "./models/public-key"
 import PublicKeyPurpose from "./enums/public-key-purpose"
+import Service from "./models/service"
 import SidetreeDid from "./sidetree-did"
 import SidetreeKey from "./sidetree-key"
 import SidetreeRequest from "./sidetree-request"
@@ -16,14 +16,14 @@ import SidetreeSdkConfig from "./sidetree-sdk-config"
 
 export {
     ISigner,
-    IonDocumentModel,
-    IonPublicKeyModel,
+    Document,
+    PublicKey,
     PublicKeyPurpose,
     SidetreeKey,
     SidetreeDid,
     SidetreeRequest,
     SidetreeSdkConfig,
-    IonServiceModel,
+    Service,
     JwkEd25519,
     JwkEs256k,
     LocalSigner,
